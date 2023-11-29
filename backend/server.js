8932319d8ba07b896111ce48fb2d53b6ca890265
Tanmesh <tanmeshnm@gmail.com>
2023-11-28 20:44:59 -0700

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
+const path = require('path')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 
@@ -21,6 +22,21 @@ app.use(express.urlencoded({ extended: false })) // allows to receive  data
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 app.use('/api/notes', require('./routes/noteRoutes'))
+
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    // Set build folder as static folder
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../', 'frontend', 'build', 'index.html'))
+    })
+} else {
+    app.get('*', (req, res) => {
+        res.status(200).json({ message: 'Welcome to the Support Desk API' })
+    })
+}
 
 app.use(errorHandler)
 
